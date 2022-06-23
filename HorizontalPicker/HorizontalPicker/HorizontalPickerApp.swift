7003e73a65f2563ac3a901e04c8fb07d381da3e6
Doganay <96667197+doganaybumbaye@users.noreply.github.com>
2022-06-24 01:47:37 +0300

    still don't know what's the difference hori\vert
    previous one deleted cause wrong named files

---
+++ HorizontalPicker/HorizontalPicker/HorizontalPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalPickerApp.swift
+//  VerticalPicker
+//
+//  Created by Doğanay Şahin on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
